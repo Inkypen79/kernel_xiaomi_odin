7afe126e9c60f12c232fe22cabb1c3577095524b
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-26 08:38:53 +0300

    techpack: display: protect setting fod hbm status by allow_bl_update
    In the current implementation the fod hbm command might set the backlight
    of the panel before receiving a frame from the driver during the first commit.
    There is chance for garbage content to be shown onto the panel.
    This change imposes a condition to delay the fod hbm command until the
    first frame is received from the HW.

--- techpack/display/msm/sde/sde_connector.c
+++ techpack/display/msm/sde/sde_connector.c
@@ -907,6 +907,7 @@ void sde_connector_fod_pre_kickoff(struct drm_connector *connector)
 	struct sde_connector *c_conn;
 	struct dsi_display *display;
 	struct dsi_panel *panel;
+	int rc;
 
 	if (!connector)
 		return;
@@ -929,7 +930,9 @@ void sde_connector_fod_pre_kickoff(struct drm_connector *connector)
 	if (dsi_panel_is_fod_hbm_applied(panel))
 		return;
 
-	dsi_panel_apply_requested_fod_hbm(panel);
+	rc = dsi_panel_apply_requested_fod_hbm(panel);
+	if (rc)
+		return;
 
 	if (!dsi_panel_get_fod_hbm(panel))
 		dsi_panel_set_fod_ui(panel, 0);
