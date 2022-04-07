7b1ece88d5f0c9637e8c94ead21177822cfb7fe4
EcrosoftXiao <xy19990903@gmail.com>
2022-04-08 03:32:28 +0800

    drivers: power: supply: Fix TIME_TO_FULL_* property on MIUI 13 firmware

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1750,6 +1750,9 @@ static int battery_psy_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX:
 		pval->intval = bcdev->num_thermal_levels;
 		break;
+	case POWER_SUPPLY_PROP_TIME_TO_FULL_AVG:
+		pval->intval = (pst->prop[prop_id] * 30) >= 65535 ? 65535 : (pst->prop[prop_id] * 30);
+		break;
 	default:
 		pval->intval = pst->prop[prop_id];
 		break;
