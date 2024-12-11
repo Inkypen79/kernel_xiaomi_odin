bdb7aa8e909531628d6d89ed0ce59c7a16741577
Giovanni Ricca <giovanniricca@proton.me>
2024-12-11 11:41:32 +0100

    Revert "disp: msm: dp: disable pluggable eDP display backlight config"
    This reverts commit bd47e3e63b26936876524406bf4172f051f2c949.
    
    Change-Id: I45fc13b3195d8d2b87a00e632e78c79a33c2c31c

--- techpack/display/msm/dp/dp_parser.h
+++ techpack/display/msm/dp/dp_parser.h
@@ -232,7 +232,6 @@ static inline char *dp_phy_aux_config_type_to_string(u32 cfg_type)
  * @has_mst_sideband: MST sideband feature enable status
  * @no_aux_switch: presence AUX switch status
  * @gpio_aux_switch: presence GPIO AUX switch status
- * @no_backlight_support: For some display type that no support backlight
  * @dsc_feature_enable: DSC feature enable status
  * @fec_feature_enable: FEC feature enable status
  * @dsc_continuous_pps: PPS sent every frame by HW
@@ -274,7 +273,6 @@ struct dp_parser {
 	bool gpio_aux_switch;
 	bool lphw_hpd;
 	bool panel_notifier_support;
-	bool no_backlight_support;
 	u32 bl_min_level;
 	u32 bl_max_level;
 	u32 brightness_max_level;
