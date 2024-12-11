dc94b5ccae40441d2a8e199c04e7502ec411ce25
Giovanni Ricca <giovanniricca@proton.me>
2024-12-11 11:40:52 +0100

    Revert "disp: msm: dp: add support of hotplug for eDP interface."
    This reverts commit 0f45eca383838f4d20955d11f6058e70052a6cad.
    
    Change-Id: Ib5b13045e439f502db75e9d803362400fe6fe360

--- techpack/display/msm/dp/dp_display.c
+++ techpack/display/msm/dp/dp_display.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
  */
 
@@ -2119,8 +2119,6 @@ static int dp_init_sub_modules(struct dp_display_private *dp)
 		goto error;
 	}
 
-	dp->parser->is_edp = dp->dp_display.is_edp;
-
 	rc = dp->parser->parse(dp->parser);
 	if (rc) {
 		DP_ERR("device tree parsing failed\n");
@@ -2131,7 +2129,6 @@ static int dp_init_sub_modules(struct dp_display_private *dp)
 	dp->dp_display.dsc_cont_pps = dp->parser->dsc_continuous_pps;
 
 	dp->dp_display.no_backlight_support = dp->parser->no_backlight_support;
-	dp->dp_display.ext_hpd_en = dp->parser->ext_hpd_en;
 	dp->catalog = dp_catalog_get(dev, dp->parser);
 	if (IS_ERR(dp->catalog)) {
 		rc = PTR_ERR(dp->catalog);
@@ -3925,8 +3922,8 @@ static int dp_display_get_display_type(struct dp_display *dp_display,
 	}
 
 	dp = container_of(dp_display, struct dp_display_private, dp_display);
-	if (dp->parser)
-		*display_type = dp->parser->display_type;
+
+	*display_type = dp->parser->display_type;
 
 	return 0;
 }
