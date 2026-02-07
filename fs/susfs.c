bcd51d52c65777fe83397b5853bfdc977d24ec74
Nicholas Magill <nmagill1979@gmail.com>
2026-02-07 14:38:12 +0000

    fs: susfs: Use handle_event instead of handle_inode_event

--- fs/susfs.c
+++ fs/susfs.c
@@ -1031,9 +1031,11 @@ static int watch_one_dir(struct watch_dir *wd)
 	return 0;
 }
 
-static int susfs_handle_sdcard_inode_event(struct fsnotify_mark *mark, u32 mask,
-											struct inode *inode, struct inode *dir,
-											const struct qstr *file_name, u32 cookie)
+static int susfs_handle_sdcard_inode_event(struct fsnotify_group *group,
+										   struct inode *inode,
+										   u32 mask, const void *data, int data_type,
+										   const struct qstr *file_name, u32 cookie,
+										   struct fsnotify_iter_info *iter_info)
 {
 	static bool target_path_is_found = false;
 
@@ -1060,7 +1062,7 @@ static int susfs_handle_sdcard_inode_event(struct fsnotify_mark *mark, u32 mask,
 }
 
 static const struct fsnotify_ops fsnotify_ops = {
-	.handle_inode_event = susfs_handle_sdcard_inode_event,
+	.handle_event = susfs_handle_sdcard_inode_event,
 };
 
 static int add_mark_on_inode(struct inode *inode, u32 mask,
